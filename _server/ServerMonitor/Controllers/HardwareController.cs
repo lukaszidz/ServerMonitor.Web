231b89f832ed6be8b0b309b7d91907014533f98c
lukasz.idziak <lukasz.idziak@fenergo.com>
2019-03-14 10:00:29 +0100

    Revert "start with mediatR"
    This reverts commit e7e0d37da28a0462d3bcef3849aea5e8099838b5.

--- _server/ServerMonitor/Controllers/HardwareController.cs
+++ _server/ServerMonitor/Controllers/HardwareController.cs
@@ -1,44 +1,30 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using System.Collections.Generic;
 using System.Web.Http;
-using MediatR;
-using Microsoft.VisualBasic.Devices;
+using Newtonsoft.Json.Linq;
 using ServerMonitor.Helpers;
 using ServerMonitor.Models;
-using ServerMonitor.Queries;
+using static ServerMonitor.Helpers.HardwareManager;
 
 namespace ServerMonitor.Controllers
 {
     public class HardwareController : BaseApi
     {
-        protected static PerformanceCounter CpuCounter { get; set; }
-
-        private readonly ComputerInfo _computerInfo = new ComputerInfo();
-        private readonly DriveInfo _driveInfo = DriveInfo.GetDrives().First(x => x.Name == "C:\\");
-        private readonly IMediator _mediator;
-
-        public HardwareController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
-        [Route("Hardware/Get")]
         public Response Get()
         {
             var response = new Response();
             try
             {
-                response.Data = _mediator.Send(new HardwareQuery()).Result;
+                var hardware = CacheManager.GetObjectFromCache("IISApplications", _cacheLifecycle, GetHardware);
+                response.Data = hardware;
                 return response;
             }
             catch (Exception ex)
             {
                 Log.Error(ex.Message);
                 response.Status = Status.Error;
-                response.AddErrorNotification(ex.Message, ex.StackTrace);
+                response.AddErrorNotification(ex.Message,ex.StackTrace);
                 return response;
             }
         }
@@ -48,8 +34,37 @@ namespace ServerMonitor.Controllers
         public Response GetAll()
         {
             var response = new Response();
-            response.Data = _mediator.Send(new HardwareQuery(getAll: true)).Result;
+            var harwareList = new List<Hardware>();
+
+            var config = LinksHelper.GetLinks("hardwareList");
+
+            if (config == null)
+            {
+                response.Status = Status.Error;
+                response.AddErrorNotification("Configuration of hardwareList missing");
+                return response;
+            }
+
+            foreach (LinkItem link in config)
+            {
+                var linkUrl = $"{link.Url.EnsureSlash()}hardware/get";
+                var responseItem = ApiClient.Get<Response>(linkUrl);
+                if (responseItem.Status == Status.Success)
+                {
+                    var innerResponse = (Response) responseItem.Data;
+                    response.Notifications.AddRange(innerResponse.Notifications);
+                    if (innerResponse.Status == Status.Success)
+                    {
+                        var data = ((JObject) innerResponse.Data).ToObject<Hardware>();
+                        harwareList.Add(data);
+                    }
+                }
+                response.Notifications.AddRange(responseItem.Notifications);
+            }
+
+            response.Data = harwareList;
             return response;
         }
     }
+
 }
--- _server/ServerMonitor/Helpers/HardwareManager.cs
+++ _server/ServerMonitor/Helpers/HardwareManager.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Management;
 using Microsoft.VisualBasic.Devices;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using ServerMonitor.Models;
 
 namespace ServerMonitor.Helpers
 {
@@ -15,53 +11,20 @@ namespace ServerMonitor.Helpers
     {
         private static PerformanceCounter CpuCounter { get; set; }
         private static readonly ComputerInfo ComputerInfo = new ComputerInfo();
-        private static readonly DriveInfo DriveInfo = DriveInfo.GetDrives().First(x => x.Name == "C:\\");
 
         public static Hardware GetHardware()
         {
             var hardware = new Hardware
             {
                 Name = ComputerName(),
-                Data = new List<Data<double>>
+                Data = new List<KeyValueData<double>>
                 {
-                    new Data<double> {Name = "CPU", Value = CpuUsage()},
-                    new Data<double> {Name = "RAM", Value = MemoryUsage(), Text = TotalMemory().ToString()},
-                    new Data<double> {Name = "HDD", Value = DiskUsage(), Text = TotalDiskSpace().ToString()}
+                    new KeyValueData<double> { Key = "CPU", Value = CpuUsage()},
+                    new KeyValueData<double> { Key = "RAM", Value = MemoryUsage()},
+                    new KeyValueData<double> { Key = "HDD", Value = DiskUsage()}
                 }
             };
             return hardware;
-        }
-
-        public static List<Hardware> GetAll()
-        {
-            var hardwareList = new List<Hardware>();
-            var config = LinksHelper.GetLinks("hardwareList");
-
-            if (config == null)
-            {
-                //TO RETHINK: passing value to the response
-                //response.Status = Status.Error;
-                //response.AddErrorNotification("Configuration of hardwareList missing");
-                //return response;
-                throw new NullReferenceException("Configuration of hardwareList missing");
-            }
-
-            foreach (LinkItem link in config)
-            {
-                var linkUrl = $"{link.Url.EnsureSlash()}hardware/get";
-                var responseItem = ApiClient.Get<Response>(linkUrl);
-                if (responseItem.Status == Status.Success)
-                {
-                    var innerResponse = (Response)responseItem.Data;
-                    if (innerResponse.Status == Status.Success)
-                    {
-                        var data = ((JObject)innerResponse.Data).ToObject<Hardware>();
-                        hardwareList.Add(data);
-                    }
-                }
-            }
-
-            return hardwareList;
         }
 
         private static double CpuUsage()
@@ -102,34 +65,13 @@ namespace ServerMonitor.Helpers
             return string.Empty;
         }
 
-        private static ulong TotalMemory()
-        {
-            return ComputerInfo.TotalPhysicalMemory;
-        }
-
-        private static long TotalDiskSpace()
-        {
-            return DriveInfo.TotalSize;
-        }
-
         [JsonObject(MemberSerialization.OptIn)]
         public class Hardware
         {
             [JsonProperty("key")]
             public string Name { get; set; }
             [JsonProperty("data")]
-            public List<Data<double>> Data { get; set; }
-        }
-
-        [JsonObject(MemberSerialization.OptIn)]
-        public class Data<T>
-        {
-            [JsonProperty("key")]
-            public string Name { get; set; }
-            [JsonProperty("value")]
-            public T Value { get; set; }
-            [JsonProperty("text")]
-            public string Text { get; set; }
+            public List<KeyValueData<double>> Data { get; set; }
         }
     }
 }
